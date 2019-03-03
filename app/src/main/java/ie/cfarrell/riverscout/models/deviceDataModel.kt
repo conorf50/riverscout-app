e57eb97953c02817d9cbfdc333b41baa422ac76b
Conor <conorf50@gmail@.com>
2019-03-03 17:28:50 +0000

    added models for device list and data

---
+++ app/src/main/java/ie/cfarrell/riverscout/models/deviceDataModel.kt
@@ -0,0 +1,5 @@
+package ie.cfarrell.riverscout.models
+
+data class deviceDataModel (var _id :String, var deviceID: String, var waterLevel : String, var waterTemp :String, var timestamp : String){
+    
+}
---
+++ app/src/main/java/ie/cfarrell/riverscout/models/deviceListModel.kt
@@ -0,0 +1,11 @@
+package ie.cfarrell.riverscout.models
+/*
+    Special thanks to Brandan Jones' tutorial 'Use Retrofit to parse JSON in Kotlin Android app'
+    https://www.youtube.com/watch?v=FW7sY7M_E8k
+
+ */
+
+
+data class deviceListModel(var _id : String, var displayName : String ) {
+
+}
