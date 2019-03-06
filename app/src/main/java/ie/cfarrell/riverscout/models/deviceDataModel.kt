8ee8313dfc59791d34785b5323c103500fcdc18b
Conor <conorf50@gmail@.com>
2019-03-06 12:39:30 +0000

    make data classes parcelable

--- app/src/main/java/ie/cfarrell/riverscout/models/deviceDataModel.kt
+++ app/src/main/java/ie/cfarrell/riverscout/models/deviceDataModel.kt
@@ -1,10 +1,12 @@
 package ie.cfarrell.riverscout.models
+
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 /*
     Special thanks to Brandan Jones' tutorial 'Use Retrofit to parse JSON in Kotlin Android app'
     https://www.youtube.com/watch?v=FW7sY7M_E8k
 
  */
-
-data class deviceDataModel (var _id :String, var deviceID: String, var waterLevel : String, var waterTemp :String, var timestamp : String){
-
-}
+@Parcelize
+data class deviceDataModel (var _id :String, var deviceID: String, var waterLevel : String, var waterTemp :String, var timestamp : String) : Parcelable
