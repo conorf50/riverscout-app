9977d5a362e2344082f074a78943a2fd5e45e4c1
Conor <conorf50@gmail@.com>
2019-04-08 21:57:27 +0100

    updated URL

--- app/src/main/java/ie/cfarrell/riverscout/interfaces/RetrofitClientInstance.kt
+++ app/src/main/java/ie/cfarrell/riverscout/interfaces/RetrofitClientInstance.kt
@@ -17,7 +17,7 @@ object RetrofitClientInstance {
 
     private var retrofit: Retrofit? = null
     //val base_url: String = getString(R.string.remote_rest_url)
-    private val BASE_URL = "http://18.202.34.190:8080/"
+    private val BASE_URL = "http://35.246.23.66:8080/"
     //private val BASE_URL = ;
 
 
